73c026b8ce43fb95abe3012781c1c36a30bf34b5
Genobabeno <jacobdgeno@gmail.com>
2025-06-21 23:56:31 -0500

    Calendar screen fully implemented, app still needs adjustments and refactoring. +Organized file structure +Componentized Calendar & navigation +Top toolbar

---
+++ app/src/main/java/com/teamjg/dreamsanddoses/navigation/AppNavigation.kt
@@ -0,0 +1,43 @@
+package com.teamjg.dreamsanddoses.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.teamjg.dreamsanddoses.uis.*
+import com.teamjg.dreamsanddoses.uis.calendarUI.CalendarScreen
+
+
+/* Centralized object to store route names */
+object Routes {
+    const val HOME = "home"
+    const val JOURNAL = "journal"
+    const val CALENDAR = "calendar"
+    const val SETTINGS = "settings"
+    const val PILLS = "pills"
+    const val FILES = "files"
+    const val DREAMS = "dreams"
+
+}
+
+//Main navigation host, managing all top-level screens
+@Composable
+fun AppNavigation(
+    navController: NavHostController = rememberNavController()
+) {
+    // Sets up the navigation graph with a start destination
+    NavHost(
+        navController = navController,
+        startDestination = Routes.HOME
+    ) {
+        // Each screen is added as a route with its corresponding composable
+        composable(Routes.HOME) { HomeScreen(navController) }
+        composable(Routes.JOURNAL) { JournalScreen(navController) }
+        composable(Routes.CALENDAR) { CalendarScreen(navController) }
+        composable(Routes.SETTINGS) { SettingsScreen(navController) }
+        composable(Routes.PILLS) { PillsScreen(navController) }
+        composable(Routes.FILES) { FilesScreen(navController) }
+        composable(Routes.DREAMS) { DreamsScreen(navController) }
+    }
+}
